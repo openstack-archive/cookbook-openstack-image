9ef2529afd0e21cd46d1a96f9e7088e816bebd2e
John Dewey <john@dewey.ws>
2013-01-03 22:06:15 -0800

    Ooops duplicate name attribute
    Didn't see it before.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ license           "Apache 2.0"
 description       "Installs and configures the Glance Image Registry and Delivery Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "2012.2.0"
-name              "glance"
 recipe            "glance::api", "Installs packages required for a glance api server"
 recipe            "glance::registry", "Installs packages required for a glance registry server"
 recipe            "glance::db", "Creates the Glance registry database"
