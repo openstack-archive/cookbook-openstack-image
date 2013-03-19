cd123e79f4684409454b7e3d935b71b00246da4f
Jay Pipes <jaypipes@gmail.com>
2013-03-19 17:24:39 -0400

    Couple typos in variable copy/paste

--- recipes/keystone_registration.rb
+++ recipes/keystone_registration.rb
@@ -31,6 +31,11 @@ auth_uri = ::URI.decode identity_admin_endpoint.to_s
 registry_endpoint = endpoint "image-registry"
 api_endpoint = endpoint "image-api"
 
+service_pass = service_password "glance"
+service_tenant_name = node["glance"]["service_tenant_name"]
+service_user = node["glance"]["service_user"]
+service_role = node["glance"]["service_role"]
+
 # Register Image Service
 keystone_register "Register Image Service" do
   auth_uri auth_uri
@@ -59,7 +64,7 @@ end
 keystone_register "Register Service Tenant" do
   auth_uri auth_uri
   bootstrap_token bootstrap_token
-  tenant_name node["glance"]["service_tenant_name"]
+  tenant_name service_tenant_name
   tenant_description "Service Tenant"
   tenant_enabled "true" # Not required as this is the default
 
@@ -70,7 +75,7 @@ end
 keystone_register "Register #{service_user} User" do
   auth_uri auth_uri
   bootstrap_token bootstrap_token
-  tenant_name node["glance"]["service_tenant_name"]
+  tenant_name service_tenant_name
   user_name service_user
   user_pass service_pass
   user_enabled "true" # Not required as this is the default
