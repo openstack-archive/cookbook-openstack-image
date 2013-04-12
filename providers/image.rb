24c6d12c302bf588fcc7389ffcac6fd2b1d84c54
Jay Pipes <jaypipes@gmail.com>
2013-04-12 13:18:34 -0400

    Reversed resource and provider :(

--- providers/image.rb
+++ providers/image.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: glance
-# Resource:: image
+# Provider:: image
 #
 # Copyright 2012, Rackspace US, Inc.
 #
@@ -17,19 +17,94 @@
 # limitations under the License.
 #
 
-actions :upload
+action :upload do
+  @user = new_resource.keystone_user
+  @pass = new_resource.keystone_pass
+  @tenant = new_resource.keystone_tenant
+  @ks_uri = new_resource.keystone_uri
+  @glance_cmd = "glance --insecure -I #{@user} -K #{@pass} -T #{@tenant} -N #{@ks_uri}"
 
-# In earlier versions of Chef the LWRP DSL doesn't support specifying
-# a default action, so you need to drop into Ruby.
-def initialize(*args)
-  super
-  @action = :upload
+  name = new_resource.image_name
+  url = new_resource.image_url
+  type = new_resource.image_type
+  if type == "unknown"
+    type = _determine_type(url)
+  end
+  _upload_image(type, name, url)
+  new_resource.updated_by_last_action(true)
 end
 
-attribute :image_url, :kind_of => String
-attribute :image_type, :kind_of => String, :default => "unknown", :equal_to => ["unknown", "ami", "qcow"]
-attribute :image_name, :kind_of => String, :default => "default"
-attribute :keystone_user, :kind_of => String
-attribute :keystone_pass, :kind_of => String
-attribute :keystone_tenant, :kind_of => String
-attribute :keystone_uri, :kind_of => String
+private
+def _determine_type(url)
+  # Lets do our best to determine the type from the file extension
+  case ::File.extname(url)
+  when ".gz", ".tgz"
+    return "ami"
+  when ".qcow2", ".img"
+    return "qcow"
+  end
+end
+
+private
+def _upload_image(type, name, url)
+  case type
+  when 'ami'
+    _upload_ami(name, url)
+  when 'qcow'
+    _upload_qcow(name, url)
+  end
+end
+
+private
+def _upload_qcow(name, url)
+  c_fmt = "--container-format bare"
+  d_fmt = "--disk-format qcow2"
+
+  bash "Uploading QCOW2 image #{name}" do
+    cwd "/tmp"
+    user "root"
+    code <<-EOH
+        #{@glance_cmd} image-create --name "#{name}" --is-public true #{c_fmt} #{d_fmt} --location "#{url}"
+    EOH
+    not_if "#{@glance_cmd} image-list | grep #{name.to_s}"
+  end
+end
+
+private
+def _upload_ami(name, url)
+  aki_fmt = "--container-format aki --disk-format aki"
+  ari_fmt = "--container-format ari --disk-format ari"
+  ami_fmt = "--container-format ami --disk-format ami"
+
+  bash "Uploading AMI image #{name}" do
+    cwd "/tmp"
+    user "root"
+    code <<-EOH
+        set -x
+        mkdir -p images/#{name}
+        cd images/#{name}
+
+        curl -L #{url} | tar -zx
+        image_name=$(basename #{url} .tar.gz)
+
+        image_name=${image_name%-multinic}
+
+        kernel_file=$(ls *vmlinuz-virtual | head -n1)
+        if [ ${#kernel_file} -eq 0 ]; then
+            kernel_file=$(ls *vmlinuz | head -n1)
+        fi
+
+        ramdisk=$(ls *-initrd | head -n1)
+        if [ ${#ramdisk} -eq 0 ]; then
+            ramdisk=$(ls *-loader | head -n1)
+        fi
+
+        kernel=$(ls *.img | head -n1)
+
+        kid=$(#{@glance_cmd} image-create --name "${image_name}-kernel" --is-public true #{aki_fmt} < ${kernel_file} | cut -d: -f2 | sed 's/ //')
+        rid=$(#{@glance_cmd} image-create --name "${image_name}-initrd" --is-public true #{ari_fmt} < ${ramdisk} | cut -d: -f2 | sed 's/ //')
+        #{@glance_cmd} image-create --name "#{name}" --is-public true #{ami_fmt} --property "kernel_id=$kid" --property "ramdisk_id=$rid" < ${kernel}
+    EOH
+    not_if "#{@glance_cmd} image-list | grep #{name.to_s}"
+  end
+end
