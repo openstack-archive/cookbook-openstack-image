8410f9a799f9963766ae99ebf06f166dfb5ccf2d
John Dewey <john@dewey.ws>
2013-05-24 12:17:28 -0700

    Switch to Semantic Versioning
    Configure the image coobook with the new versioning scheme we
    discusssed on the ML. It will start out at 7.0.0(7th OpenStack
    release).
    
    Change-Id: I4a359fd78b337b900b24c2ed9c4be3399a3fc526
    Fixes: Bug 1183974

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Glance Image Registry and Delivery Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2012.2.3"
+version           "7.0.0"
 recipe            "openstack-image::api", "Installs packages required for a glance api server"
 recipe            "openstack-image::registry", "Installs packages required for a glance registry server"
 recipe            "openstack-image::db", "Creates the Glance registry database"
