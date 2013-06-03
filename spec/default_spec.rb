fcf0865d63e386ff3d0fe383ea33ef82c41f9a61
John Dewey <john@dewey.ws>
2013-06-03 11:14:58 -0700

    Tests should be using require_relative
    The #require_relative statement is relative to the file
    containing the the statement. Also, updated spec_helper
    constants to follow existing spacing format.
    
    Change-Id: I61bf376929ba23a4743474695d22b8d5c7494af5

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require_relative "spec_helper"
 
 describe "openstack-dashboard::default" do
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,14 @@ require "chefspec"
 
 ::LOG_LEVEL = :fatal
 ::REDHAT_OPTS = {
-    :platform  => "redhat",
-    :log_level => ::LOG_LEVEL
+  :platform  => "redhat",
+  :version   => "6.3",
+  :log_level => ::LOG_LEVEL
 }
 ::UBUNTU_OPTS = {
-    :platform  => "ubuntu",
-    :version   => "12.04",
-    :log_level => ::LOG_LEVEL
+  :platform  => "ubuntu",
+  :version   => "12.04",
+  :log_level => ::LOG_LEVEL
 }
 
 def image_stubs
