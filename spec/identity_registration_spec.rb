b0089fbe82ecc8c981f862ba6afeea6c4ed2f6bb
John Dewey <john@dewey.ws>
2013-06-08 21:33:43 -0700

    Clearer registraton LWRP tests
    This is a much clearer way to test LWRPs are executed with
    the proper options.  The previous approach was hacky, and
    relied upon too much stubbing and ruby magik.  Also, updated
    to berkshelf 2.0 where Berksfile.lock is respected.  This
    is necessary for berkshelf to lock to the proper cookbook
    deps in openstack's CI system.
    
    Change-Id: Ia912efffa137a75352423e7571cb74bca9ab1b5d

--- spec/identity_registration_spec.rb
+++ spec/identity_registration_spec.rb
@@ -3,132 +3,89 @@ require_relative "spec_helper"
 describe "openstack-image::identity_registration" do
   before do
     image_stubs
-    @identity_register_mock = double "identity_register"
+    @chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
+    @chef_run.converge "openstack-image::identity_registration"
   end
 
   it "registers image service" do
-    ::Chef::Recipe.any_instance.stub(:openstack_identity_register)
-    ::Chef::Recipe.any_instance.should_receive(:openstack_identity_register).
-      with("Register Image Service") do |&arg|
-        @identity_register_mock.should_receive(:auth_uri).
-          with "https://127.0.0.1:35357/v2.0"
-        @identity_register_mock.should_receive(:bootstrap_token).
-          with "bootstrap-token"
-        @identity_register_mock.should_receive(:service_name).
-          with "glance"
-        @identity_register_mock.should_receive(:service_type).
-          with "image"
-        @identity_register_mock.should_receive(:service_description).
-          with "Glance Image Service"
-        @identity_register_mock.should_receive(:action).
-          with :create_service
+    resource = @chef_run.find_resource(
+      "openstack-identity_register",
+      "Register Image Service"
+    ).to_hash
 
-        @identity_register_mock.instance_eval &arg
-      end
-
-    chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
-    chef_run.converge "openstack-image::identity_registration"
+    expect(resource).to include(
+      :auth_uri => "https://127.0.0.1:35357/v2.0",
+      :bootstrap_token => "bootstrap-token",
+      :service_type => "image",
+      :service_description => "Glance Image Service",
+      :action => [:create_service]
+    )
   end
 
   it "registers image endpoint" do
-    ::Chef::Recipe.any_instance.stub(:openstack_identity_register)
-    ::Chef::Recipe.any_instance.should_receive(:openstack_identity_register).
-      with("Register Image Endpoint") do |&arg|
-        @identity_register_mock.should_receive(:auth_uri).
-          with "https://127.0.0.1:35357/v2.0"
-        @identity_register_mock.should_receive(:bootstrap_token).
-          with "bootstrap-token"
-        @identity_register_mock.should_receive(:service_type).
-          with "image"
-        @identity_register_mock.should_receive(:endpoint_region).
-          with "RegionOne"
-        @identity_register_mock.should_receive(:endpoint_adminurl).
-          with "https://127.0.0.1:9292/v2"
-        @identity_register_mock.should_receive(:endpoint_internalurl).
-          with "https://127.0.0.1:9292/v2"
-        @identity_register_mock.should_receive(:endpoint_publicurl).
-          with "https://127.0.0.1:9292/v2"
-        @identity_register_mock.should_receive(:action).
-          with :create_endpoint
+    resource = @chef_run.find_resource(
+      "openstack-identity_register",
+      "Register Image Endpoint"
+    ).to_hash
 
-        @identity_register_mock.instance_eval &arg
-      end
-
-    chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
-    chef_run.converge "openstack-image::identity_registration"
+    expect(resource).to include(
+      :auth_uri => "https://127.0.0.1:35357/v2.0",
+      :bootstrap_token => "bootstrap-token",
+      :service_type => "image",
+      :endpoint_region => "RegionOne",
+      :endpoint_adminurl => "https://127.0.0.1:9292/v2",
+      :endpoint_internalurl => "https://127.0.0.1:9292/v2",
+      :endpoint_publicurl => "https://127.0.0.1:9292/v2",
+      :action => [:create_endpoint]
+    )
   end
 
   it "registers service tenant" do
-    ::Chef::Recipe.any_instance.stub(:openstack_identity_register)
-    ::Chef::Recipe.any_instance.should_receive(:openstack_identity_register).
-      with("Register Service Tenant") do |&arg|
-        @identity_register_mock.should_receive(:auth_uri).
-          with "https://127.0.0.1:35357/v2.0"
-        @identity_register_mock.should_receive(:bootstrap_token).
-          with "bootstrap-token"
-        @identity_register_mock.should_receive(:tenant_name).
-          with "service"
-        @identity_register_mock.should_receive(:tenant_description).
-          with "Service Tenant"
-        @identity_register_mock.should_receive(:tenant_enabled).
-          with true
-        @identity_register_mock.should_receive(:action).
-          with :create_tenant
+    resource = @chef_run.find_resource(
+      "openstack-identity_register",
+      "Register Service Tenant"
+    ).to_hash
 
-        @identity_register_mock.instance_eval &arg
-      end
-
-    chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
-    chef_run.converge "openstack-image::identity_registration"
+    expect(resource).to include(
+      :auth_uri => "https://127.0.0.1:35357/v2.0",
+      :bootstrap_token => "bootstrap-token",
+      :tenant_name => "service",
+      :tenant_description => "Service Tenant",
+      :tenant_enabled => true,
+      :action => [:create_tenant]
+    )
   end
 
   it "registers service user" do
-    ::Chef::Recipe.any_instance.stub(:openstack_identity_register)
-    ::Chef::Recipe.any_instance.should_receive(:openstack_identity_register).
-      with("Register glance User") do |&arg|
-        @identity_register_mock.should_receive(:auth_uri).
-          with "https://127.0.0.1:35357/v2.0"
-        @identity_register_mock.should_receive(:bootstrap_token).
-          with "bootstrap-token"
-        @identity_register_mock.should_receive(:tenant_name).
-          with "service"
-        @identity_register_mock.should_receive(:user_name).
-          with "glance"
-        @identity_register_mock.should_receive(:user_pass).
-          with "glance-pass"
-        @identity_register_mock.should_receive(:user_enabled).
-          with true
-        @identity_register_mock.should_receive(:action).
-          with :create_user
+    resource = @chef_run.find_resource(
+      "openstack-identity_register",
+      "Register glance User"
+    ).to_hash
 
-        @identity_register_mock.instance_eval &arg
-      end
-
-    chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
-    chef_run.converge "openstack-image::identity_registration"
+    expect(resource).to include(
+      :auth_uri => "https://127.0.0.1:35357/v2.0",
+      :bootstrap_token => "bootstrap-token",
+      :tenant_name => "service",
+      :user_name => "glance",
+      :user_pass => "glance-pass",
+      :user_enabled => true,
+      :action => [:create_user]
+    )
   end
 
   it "grants admin role to service user for service tenant" do
-    ::Chef::Recipe.any_instance.stub(:openstack_identity_register)
-    ::Chef::Recipe.any_instance.should_receive(:openstack_identity_register).
-      with("Grant 'admin' Role to glance User for service Tenant") do |&arg|
-        @identity_register_mock.should_receive(:auth_uri).
-          with "https://127.0.0.1:35357/v2.0"
-        @identity_register_mock.should_receive(:bootstrap_token).
-          with "bootstrap-token"
-        @identity_register_mock.should_receive(:tenant_name).
-          with "service"
-        @identity_register_mock.should_receive(:role_name).
-          with "admin"
-        @identity_register_mock.should_receive(:user_name).
-          with "glance"
-        @identity_register_mock.should_receive(:action).
-          with :grant_role
+    resource = @chef_run.find_resource(
+      "openstack-identity_register",
+      "Grant 'admin' Role to glance User for service Tenant"
+    ).to_hash
 
-        @identity_register_mock.instance_eval &arg
-      end
-
-    chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
-    chef_run.converge "openstack-image::identity_registration"
+    expect(resource).to include(
+      :auth_uri => "https://127.0.0.1:35357/v2.0",
+      :bootstrap_token => "bootstrap-token",
+      :tenant_name => "service",
+      :role_name => "admin",
+      :user_name => "glance",
+      :action => [:grant_role]
+    )
   end
 end
