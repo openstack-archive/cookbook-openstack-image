5ef6f9416ca7328f815209fb52624b162629bd47
John Dewey <john@dewey.ws>
2013-06-27 12:40:48 -0700

    Removed the db recipe
    The db recipe have been removed.  It is up to the operator to
    setup the database, users, and grants before deploying openstack.
    It has been moved to the `openstack-db` recipe in the
    `openstack-ops-database` wrapper cookbook.
    
    Change-Id: I15bac0058aa0021713a95c458f70eeeac80b4760

--- metadata.rb
+++ metadata.rb
@@ -6,15 +6,11 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "7.0.0"
 recipe            "openstack-image::api", "Installs packages required for a glance api server"
 recipe            "openstack-image::registry", "Installs packages required for a glance registry server"
-recipe            "openstack-image::db", "Creates the Glance registry database"
 recipe            "openstack-image::identity_registration", "Registers Glance endpoints and service with Keystone"
 
 %w{ ubuntu fedora redhat centos suse }.each do |os|
   supports os
 end
 
-depends           "database"
 depends           "openstack-common", "~> 0.3.0"
 depends           "openstack-identity", "~> 7.0.0"
-suggests          "mysql"
-suggests          "postgresql"
