b0fc6885a97312c8c42b418b0c3bdde83a359ada
Darren Birkett <darren.birkett@gmail.com>
2014-04-30 17:18:59 +0100

    use newer 0.3.2 cirros image
    This version of cirros has a udhcp daemon that supports dhcp option 121,
    which allows passing of metadata static route via dhcp.  Important for
    testing using neutron and metadata agent.
    
    Also switch to download from new download.cirros-cloud.net location
    
    Change-Id: Ic7e6d23407a282c4320b915549b76ab7f86381f6

--- attributes/default.rb
+++ attributes/default.rb
@@ -99,7 +99,7 @@ default['openstack']['image']['upload_images'] = ['cirros']
 default['openstack']['image']['upload_image']['precise'] = 'http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-amd64-disk1.img'
 default['openstack']['image']['upload_image']['oneiric'] = 'http://cloud-images.ubuntu.com/oneiric/current/oneiric-server-cloudimg-amd64-disk1.img'
 default['openstack']['image']['upload_image']['natty'] = 'http://cloud-images.ubuntu.com/natty/current/natty-server-cloudimg-amd64-disk1.img'
-default['openstack']['image']['upload_image']['cirros'] = 'https://launchpadlibrarian.net/83305348/cirros-0.3.0-x86_64-disk.img'
+default['openstack']['image']['upload_image']['cirros'] = 'http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img'
 # more images available at https://github.com/rackerjoe/oz-image-build
 default['openstack']['image']['upload_image']['centos'] = 'http://c250663.r63.cf1.rackcdn.com/centos60_x86_64.qcow2'
 
