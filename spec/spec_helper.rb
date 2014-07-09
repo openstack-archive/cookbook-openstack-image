fc919312443273d974a00148642d2e1ed29a43c2
JJ Asghar <jj@getchef.com>
2014-07-09 13:14:11 -0500

    Updating Berks to 2.0.18
    This is to resolve the broken berks config with the supermarket push.
    This also fixes the strainerfile for the issue with the location of the
    gems and running bundle exec.
    Removed the spec_helper code for strainer to work, and updated chefspec.
    
    Change-Id: Ibfbfb57de7d9f004b31c8cb45f8d3406d9a2f302

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+require 'rspec/expectations'
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'chef/application'
@@ -110,9 +111,5 @@ shared_examples 'glance-directory' do
     end
   end
 end
-
-# README(galstrom21): This will remove any coverage warnings from
-#   dependent cookbooks
-ChefSpec::Coverage.filters << '*/openstack-image'
 
 at_exit { ChefSpec::Coverage.report! }
