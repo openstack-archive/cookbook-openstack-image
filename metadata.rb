649d8fe587e75080333a495b56ec8e3dac8c2617
Mark Vanderwiel <vanderwl@us.ibm.com>
2015-02-27 13:37:04 -0600

    Initial kilo updates
    Change-Id: Ifbe6bec1726790ec07ba8ca8a4d4f6e6908dc70f
    Partial-Bug: #1426424

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'opscode-chef-openstack@googlegroups.com'
 license           'Apache 2.0'
 description       'Installs and configures the Glance Image Registry and Delivery Service'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '10.1.0'
+version           '11.0.0'
 recipe            'openstack-image::api', 'Installs packages required for a glance api server'
 recipe            'openstack-image::client', 'Install packages required for glance client'
 recipe            'openstack-image::registry', 'Installs packages required for a glance registry server'
@@ -15,6 +15,6 @@ recipe            'openstack-image::image_upload', 'Upload image using glance im
   supports os
 end
 
-depends           'openstack-common', '>= 10.2.0'
-depends           'openstack-identity', '>= 10.0.0'
+depends           'openstack-common', '>= 11.0.0'
+depends           'openstack-identity', '>= 11.0.0'
 depends           'ceph', '~> 0.8.0'
