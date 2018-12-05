8142de7e19305bd80ac8fd0251fa1108458ef4fb
ZhijunWei <wzj334965317@outlook.com>
2018-12-04 23:20:59 -0500

    Change openstack-dev to openstack-discuss
    Change-Id: I3a42ded4e67249617fa68303bc47196aaf87807c

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'openstack-image'
 maintainer       'openstack-chef'
-maintainer_email 'openstack-dev@lists.openstack.org'
+maintainer_email 'openstack-discuss@lists.openstack.org'
 license          'Apache-2.0'
 description      'Installs and configures the Glance Image Registry and Delivery Service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
