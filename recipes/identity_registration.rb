59b082a6d081dc921c4e298d4ced82f09560700d
Lance Albertson <lance@osuosl.org>
2019-06-18 15:46:43 -0700

    Fixes to support fog-openstack-1.x
    fog-openstack-1.x already appends "auth/tokens" so we no longer need to
    do that.  In addition, comment out endpoint type until this PR [1] gets
    merged and released.
    
    [1] https://github.com/fog/fog-openstack/pull/494
    
    Depends-On: https://review.opendev.org/666176
    Change-Id: Ic80d08b6ed0180ca9513e4b4e230e787b2f5c52e
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/identity_registration.rb
+++ recipes/identity_registration.rb
@@ -47,15 +47,17 @@ service_user =
 service_role = node['openstack']['image']['service_role']
 service_domain_name = node['openstack']['image_api']['conf']['keystone_authtoken']['user_domain_name']
 region = node['openstack']['region']
-endpoint_type = node['openstack']['identity']['endpoint_type']
+# TODO(ramereth): commenting this out until
+# https://github.com/fog/fog-openstack/pull/494 gets merged and released.
+# endpoint_type = node['openstack']['identity']['endpoint_type']
 
 connection_params = {
-  openstack_auth_url:      "#{auth_url}/auth/tokens",
+  openstack_auth_url:      auth_url,
   openstack_username:      admin_user,
   openstack_api_key:       admin_pass,
   openstack_project_name:  admin_project,
   openstack_domain_name:   admin_domain,
-  openstack_endpoint_type: endpoint_type,
+  # openstack_endpoint_type: endpoint_type,
 }
 
 # Register Image Service
